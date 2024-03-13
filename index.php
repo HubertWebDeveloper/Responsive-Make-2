390c085737cc29a74a20b676ba689a499df6ec41
HubertWebDeveloper <145841547+HubertWebDeveloper@users.noreply.github.com>
2024-03-13 16:03:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h3>Food Delivery Services</h3>
+    </div>
+    <div class="main-contain">
+        <div class="sidebar">
+            <div class="side-card">
+                <img src="../MyOwnerLogo.png">
+                <p>Home</p>
+            </div>
+            <div class="side-card">
+                <img src="../MyOwnerLogo.png">
+                <p>Home</p>
+            </div>
+        </div>
+        <div class="main-side">
+            <div class="hero-section">
+                <div class="image">
+                    <img src="../hero.jpeg">
+                </div>
+                <div class="hero-content">
+                    <div>
+                        <h3>50% OFF</h3>
+                        <p>Tasty Food On Your Hand</p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="product-section">
+                <div class="category">
+                    <div class="heading">
+                        <img src="../next.png">
+                        <h3>Recommend Category</h3>
+                    </div>
+                    <div class="category-item">
+                        <div class="category-card">
+                            <p>Favorities</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Best Seller</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Near Me</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Favorities</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Best Seller</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Near Me</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Favorities</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Best Seller</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Near Me</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Favorities</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Best Seller</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Near Me</p>
+                        </div>
+                        <div class="category-card">
+                            <p>Favorities</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="product-list">
+                <div class="product-card">
+                    <div class="product-card-item">
+                        <img src="../MyOwnerLogo.png">
+                        <div class="product-card-content">
+                            <div class="content">
+                                <h3>Amazing Pizza</h3>
+                                <p>Lorem ipsum dolor sit,Lorem ipsum dolor sit</p>
+                            </div>
+                            <div class="price">
+                                <h3>30 Rwf</h3>
+                            </div>
+                        </div>
+                        <button submit="" name="">Add To Cart</button>
+                    </div>
+                </div>
+                <div class="product-card">
+                    <div class="product-card-item">
+                        <img src="../hero.jpeg">
+                        <div class="product-card-content">
+                            <div class="content">
+                                <h3>Amazing Pizza</h3>
+                                <p>Lorem ipsum dolor sit,Lorem ipsum dolor sit</p>
+                            </div>
+                            <div class="price">
+                                <h3>30 Rwf</h3>
+                            </div>
+                        </div>
+                        <button submit="" name="">Add To Cart</button>
+                    </div>
+                </div>
+                <div class="product-card">
+                    <div class="product-card-item">
+                        <img src="../MyOwnerLogo.png">
+                        <div class="product-card-content">
+                            <div class="content">
+                                <h3>Amazing Pizza</h3>
+                                <p>Lorem ipsum dolor sit,Lorem ipsum dolor sit</p>
+                            </div>
+                            <div class="price">
+                                <h3>30 Rwf</h3>
+                            </div>
+                        </div>
+                        <button submit="" name="">Add To Cart</button>
+                    </div>
+                </div>
+                <div class="product-card">
+                    <div class="product-card-item">
+                        <img src="../hero.jpeg">
+                        <div class="product-card-content">
+                            <div class="content">
+                                <h3>Amazing Pizza</h3>
+                                <p>Lorem ipsum dolor sit,Lorem ipsum dolor sit</p>
+                            </div>
+                            <div class="price">
+                                <h3>30 Rwf</h3>
+                            </div>
+                        </div>
+                        <button submit="" name="">Add To Cart</button>
+                    </div>
+                </div>
+                <div class="product-card">
+                    <div class="product-card-item">
+                        <img src="../MyOwnerLogo.png">
+                        <div class="product-card-content">
+                            <div class="content">
+                                <h3>Amazing Pizza</h3>
+                                <p>Lorem ipsum dolor sit,Lorem ipsum dolor sit</p>
+                            </div>
+                            <div class="price">
+                                <h3>30 Rwf</h3>
+                            </div>
+                        </div>
+                        <button submit="" name="">Add To Cart</button>
+                    </div>
+                </div>
+                <div class="product-card">
+                    <div class="product-card-item">
+                        <img src="../hero.jpeg">
+                        <div class="product-card-content">
+                            <div class="content">
+                                <h3>Amazing Pizza</h3>
+                                <p>Lorem ipsum dolor sit,Lorem ipsum dolor sit</p>
+                            </div>
+                            <div class="price">
+                                <h3>30 Rwf</h3>
+                            </div>
+                        </div>
+                        <button submit="" name="">Add To Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="list-side">
+            <div class="list-side-header">
+                <h3>Order Menu</h3>
+                <img src="../cart.png">
+            </div>
+            <div class="customer-address">
+                <p>Hubert hirwa</p>
+                <p>+250 781794795</p>
+                <h3>Muhanga, Cyakabiri, Karama center</h3>
+            </div>
+            <div class="card-list">
+                <div class="card">
+                    <img src="../hero.jpeg">
+                    <div>
+                       <div class="content">
+                          <p>Lorem ipsum dolor sit amnet</p>
+                          <h3>40 Rwf</h3>
+                       </div>
+                       <div class="buttons">
+                          <a href="#" class="del-btn">
+                            <img src="../delete.png">
+                          </a>
+                          <input type="number" name="" value="1">
+                          <a href="#" class="update-btn">Update</a>
+                       </div>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <img src="../hero.jpeg">
+                    <div>
+                       <div class="content">
+                          <p>Lorem ipsum dolor sit amnet</p>
+                          <h3>40 Rwf</h3>
+                       </div>
+                       <div class="buttons">
+                          <a href="#" class="del-btn">
+                            <img src="../delete.png">
+                          </a>
+                          <input type="number" name="" value="1">
+                          <a href="#" class="update-btn">Update</a>
+                       </div>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <img src="../hero.jpeg">
+                    <div>
+                       <div class="content">
+                          <p>Lorem ipsum dolor sit amnet</p>
+                          <h3>40 Rwf</h3>
+                       </div>
+                       <div class="buttons">
+                          <a href="#" class="del-btn">
+                            <img src="../delete.png">
+                          </a>
+                          <input type="number" name="" value="1">
+                          <a href="#" class="update-btn">Update</a>
+                       </div>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <img src="../hero.jpeg">
+                    <div>
+                       <div class="content">
+                          <p>Lorem ipsum dolor sit amnet</p>
+                          <h3>40 Rwf</h3>
+                       </div>
+                       <div class="buttons">
+                          <a href="#" class="del-btn">
+                            <img src="../delete.png">
+                          </a>
+                          <input type="number" name="" value="1">
+                          <a href="#" class="update-btn">Update</a>
+                       </div>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <img src="../hero.jpeg">
+                    <div>
+                       <div class="content">
+                          <p>Lorem ipsum dolor sit amnet</p>
+                          <h3>40 Rwf</h3>
+                       </div>
+                       <div class="buttons">
+                          <a href="#" class="del-btn">
+                            <img src="../delete.png">
+                          </a>
+                          <input type="number" name="" value="1">
+                          <a href="#" class="update-btn">Update</a>
+                       </div>
+                    </div>
+                    
+                </div>
+                <div class="card">
+                    <img src="../hero.jpeg">
+                    <div>
+                       <div class="content">
+                          <p>Lorem ipsum dolor sit amnet</p>
+                          <h3>40 Rwf</h3>
+                       </div>
+                       <div class="buttons">
+                          <a href="#" class="del-btn">
+                            <img src="../delete.png">
+                          </a>
+                          <input type="number" name="" value="1">
+                          <a href="#" class="update-btn">Update</a>
+                       </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="total">
+                <div class="total-card">
+                    <p>SubTotal</p>
+                    <h3>1550 Rwf</h3>
+                </div>
+                <div class="total-card">
+                    <p>Tax (10%)</p>
+                    <h3>350 Rwf</h3>
+                </div>
+                <div class="total-card">
+                    <p>Delivery Fee</p>
+                    <h3>150 Rwf</h3>
+                </div>
+            </div>
+             <a href="#" class="book-btn">Book Now</a>
+        </div>
+    </div>
+   
+</body>
+</html>
